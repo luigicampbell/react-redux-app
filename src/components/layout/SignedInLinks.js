926e79834d3468b6e81f47e3a1994d482ca878aa
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-12-15 11:09:31 -0800

    Links created. No logic for rendering yet

---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const SignedInLinks = () => {
+    return (
+        <ul className="right">
+            <li><NavLink to='/'>New Project</NavLink></li>
+            <li><NavLink to='/'>Log Out</NavLink></li>
+            <li><NavLink to='/' className='btn btn-floating pink lighten-1'>LC</NavLink></li>
+        </ul>
+    );
+}
+export default SignedInLinks;
