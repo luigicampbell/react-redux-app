374b21b97a95facfbb792328bf40db98e2e2c9d7
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-12-15 10:43:24 -0800

    moved component directories into component directory, navbar started

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Navbar = () => {
+    return (
+        <nav className="nav-wrapper grey darken-3">
+            <div className="container">
+               <Link to='/' className="brand-logo">React-Redux-Firebase Project</Link>
+            </div>
+        </nav>
+    ); // Materialize Class
+}
+
+export default Navbar;
