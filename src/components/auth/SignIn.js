5d893d6b20e62f2e56cb96dcb2ab72efda28af95
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-12-15 14:29:53 -0800

    project form created change and submit also populate state

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -22,17 +22,17 @@ class SignIn extends Component {
     return (
       <div className="container">
         
-        <form onSubmit={this.handleSubmit} className="white">
+        <form onSubmit={ this.handleSubmit } className="white">
 
             <h5 className="grey-text text-darken-3">Sign In</h5>
             <div className="input-field">
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" onChange={this.handleChange} />
+                <input type="email" id="email" onChange={ this.handleChange } />
             </div>
 
             <div className="input-field">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" onChange={this.handleChange} />
+                <input type="password" id="password" onChange={ this.handleChange } />
             </div>
 
             <div className="input-field">
