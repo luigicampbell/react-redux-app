fb383a0d6608dde4a77d44facc929c7b7ac87542
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-12-16 11:54:11 -0800

    set up thunk and project actions to create project asynchronously. Firebase call to come.

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { createProject } from '../../store/actions/projectActions';
 class CreateProject extends Component {
     state = {
         title: '',
@@ -15,7 +16,8 @@ class CreateProject extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+        // console.log(this.state);
+        this.props.createProject(this.state);
     }
 
   render() {
@@ -47,4 +49,10 @@ class CreateProject extends Component {
   }
 }
 
-export default CreateProject;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createProject: (project) => dispatch(createProject(project))
+    }
+};
+
+export default connect(null, mapDispatchToProps)(CreateProject);
