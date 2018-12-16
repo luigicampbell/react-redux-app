a55926151d0612b66f1f803d951f295db576a5e0
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-12-16 10:37:59 -0800

    added reducersand default states

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './ProjectSummary.css';
-const ProjectSummary = () => {
+const ProjectSummary = ({ project }) => {
     return (
         <div className="card z-depth-0 project-summary opac">
             <div className="boxshadow card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
+                <span className="card-title">{ project.title }</span>
                 <p>Posted by LC</p>
                 <p className="grey-text">DATE</p>
             </div>
