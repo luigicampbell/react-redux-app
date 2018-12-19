89a79bada2926d60788adf8184f563ca11f4ef1c
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-12-18 21:12:47 -0800

    hooked up firebase with redux and did basic error handling

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,18 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async call to database
-        dispatch({ type: 'CREATE_PROJECT', project });
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project, // spread operator in lieu of individual properties
+            authorFirstName: 'Luigi',
+            authorLastName: 'Campbell',
+            authorId: '0000',
+            createdAt: new Date()
+        }).then(()=> {
+            dispatch({ type: 'CREATE_PROJECT', project });
+        }).catch((err) => {
+            // console.log(err);
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+        });
     };
-}
+};
